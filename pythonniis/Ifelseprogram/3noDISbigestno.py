30e34f07e76f3dce8e0df10169d7b90adecf9304
suvendu <chandababun1@gmail.com>
2026-02-24 09:19:12 +0530

    test

---
+++ pythonniis/Ifelseprogram/3noDISbigestno.py
@@ -0,0 +1,15 @@
+print("enter three nos")
+no1=int(input())
+no2=int(input())
+no3=int(input())
+if no1>=no2:
+ if no1>=no3:
+ 	print("1st no is biger",no1)
+ else:
+ 	print("3rd no is biger",no3)
+else:
+ 	if no2>=no3:
+ 		print("2nd no is biger",no2)
+ 	else:
+ 		print("3rd no is biger",no3)
+

5fe05475f9ddf5e3057bbd098d753c15ef03f84b
suvendu <chandababun1@gmail.com>
2026-02-27 12:25:51 +0530

    function

---
+++ pythonniis/function/SI.py
@@ -0,0 +1,10 @@
+def sical(p,r,t):
+	SI=(p*t*r)/100
+	print("Simple Interest",SI)
+print("enter principle")
+p=float(input())
+print(" rate of interest")
+r=float(input())
+print("enter time")
+t=float(input())
+sical(p,r,t)
---
+++ pythonniis/function/function2.py
@@ -0,0 +1,10 @@
+def sical():
+	print("enter principle")
+	p=float(input())
+	print(" rate of interest")
+	r=float(input())
+	print("enter time")
+	t=float(input())
+	SI=(p*r*t)/100
+	print("Simple Interest",SI)
+sical()	
